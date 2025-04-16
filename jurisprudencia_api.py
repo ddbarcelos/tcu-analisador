9f364afcd1ad8ef2f334576ed7568be369d63cf8
ddbarcelos <contatoolicitante@gmail.com>
2025-04-16 09:34:25 -0300

    Add files via upload

--- jurisprudencia_api.py
+++ jurisprudencia_api.py
@@ -448,14 +448,14 @@ Isso significa que {explicacao}
         """
         hashtags = []
         
-       	# Adiciona hashtags com base nos temas
-	if 'temas' in acordao and acordao['temas']:
-   	    for tema in acordao['temas']:
+           # Adiciona hashtags com base nos temas
+    if 'temas' in acordao and acordao['temas']:
+           for tema in acordao['temas']:
                 hashtag = tema.replace(' e ', '').replace(' ', '')
                 hashtags.append(hashtag)
 
-	# Adiciona hashtags com base nos subtemas
-	if 'subtemas' in acordao and acordao['subtemas']:
-    	    for subtema in acordao['subtemas']:
-       	        hashtag = subtema.replace(' ', '')
+    # Adiciona hashtags com base nos subtemas
+    if 'subtemas' in acordao and acordao['subtemas']:
+            for subtema in acordao['subtemas']:
+                   hashtag = subtema.replace(' ', '')
                 hashtags.append(hashtag)
